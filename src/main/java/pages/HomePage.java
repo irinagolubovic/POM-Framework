50fcea060c80024a780af722a1cfcf0ad9b95566
QA academy <education@qaacademy.rs>
2026-04-09 19:41:01 +0200

    Stefan changes

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -13,6 +13,7 @@ public class HomePage extends BasePage{
 
     private By addToCartBtn = By.name("add-to-cart-sauce-labs-backpack");
     private By shopingCartIcon = By.id("shopping_cart_container");
+    private By exampleLocator = By.id("STEFAN");
 
     public void addToCartAction() {
         click(addToCartBtn);
